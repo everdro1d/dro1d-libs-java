6fe92df16068fc48fb83d49f7e76a41a5f6c1b33
everdro1d <everdro1d-github@pm.me>
2024-03-14 10:29:43 -0600

    feat: completed implementation of LocaleManager in FileChooser

--- src/com/everdro1d/libs/swing/components/FileChooser.java
+++ src/com/everdro1d/libs/swing/components/FileChooser.java
@@ -11,20 +11,54 @@ import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import java.awt.*;
 import java.io.File;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.prefs.Preferences;
+
+import static com.everdro1d.libs.swing.SwingGUI.setLookAndFeel;
 
 public class FileChooser extends JFileChooser {
     // UI text
-    private String selectButtonText = "Select";
+    private LocaleManager localeManager;
+    private String lookInLabelText = "Look In:";
+    private String saveInLabelText = "Save In:";
+    private String upFolderToolTipText = "Up One Level";
+    private String homeFolderToolTipText = "Home";
+    private String newFolderToolTipText = "Create New Folder";
+    private String listViewButtonToolTipText = "List";
+    private String detailsViewButtonToolTipText = "Details";
+    private String fileNameHeaderText = "Name";
+    private String fileSizeHeaderText = "Size";
+    private String fileTypeHeaderText = "Type";
+    private String fileDateHeaderText = "Date";
+    private String fileAttrHeaderText = "Attributes";
+    private String fileNameLabelText = "File Name:";
+    private String filesOfTypeLabelText = "Files of Type:";
+    private String openButtonText = "Open";
+    private String openButtonToolTipText = "Open selected file";
+    private String saveButtonText = "Save";
+    private String saveButtonToolTipText = "Save selected file";
+    private String directoryOpenButtonText = "Open";
+    private String directoryOpenButtonToolTipText = "Open selected directory";
+    private String approveButtonText = "Select";
     private String cancelButtonText = "Cancel";
+    private String cancelButtonToolTipText = "Cancel";
+    private String newFolderErrorText = "Error creating new folder";
+    private String extensionFilterDescription = "Files";
+    private String directoryOnlyDescription = "Directory (Folder)";
+    private String filesOnlyDescription = "Any File";
+    private String anyFileOrDirectory = "Any File or Directory (Folder)";
     private final Font font = this.getParent() != null
             ? this.getParent().getFont()
             : new Font("Tahoma", Font.PLAIN,16);
+
 
     /**
      * Create a generic file chooser dialog allowing selection of both files and directories.
      */
     public FileChooser(String path, String dialogTitle, LocaleManager localeManager) {
-        this(path, dialogTitle, true, true, false, null, false, null, localeManager);
+        this(path, dialogTitle, true, true, false,
+                null, false, null, localeManager);
     }
 
     /**
@@ -34,7 +68,8 @@ public class FileChooser extends JFileChooser {
      * @param selectFiles Whether to show files in the dialog. If false, only directories will be shown.
      */
     public FileChooser(String path, String dialogTitle, boolean selectFiles, LocaleManager localeManager) {
-        this(path, dialogTitle, selectFiles, !selectFiles, false, null, false, null, localeManager);
+        this(path, dialogTitle, selectFiles, !selectFiles, false, null,
+                false, null, localeManager);
     }
 
     /**
@@ -44,7 +79,8 @@ public class FileChooser extends JFileChooser {
      * @param extension The extension to filter files by. ex. "txt"
      */
     public FileChooser(String path, String dialogTitle, String extension, LocaleManager localeManager) {
-        this(path, dialogTitle, true, false, true, extension, false, null, localeManager);
+        this(path, dialogTitle, true, false, true, extension,
+                false, null, localeManager);
     }
 
     /**
@@ -54,8 +90,10 @@ public class FileChooser extends JFileChooser {
      * @param selectFiles Whether to show files in the dialog. If false, only directories will be shown.
      * @param customMessage Custom message to show in the description area.
      */
-    public FileChooser(String path, String dialogTitle, boolean selectFiles, String customMessage, LocaleManager localeManager) {
-        this(path, dialogTitle, selectFiles, !selectFiles, false, null, true, customMessage, localeManager);
+    public FileChooser(String path, String dialogTitle, boolean selectFiles, String customMessage,
+                       LocaleManager localeManager) {
+        this(path, dialogTitle, selectFiles, !selectFiles, false, null,
+                true, customMessage, localeManager);
     }
 
     /**
@@ -71,22 +109,32 @@ public class FileChooser extends JFileChooser {
      * @param localeManager the locale manager to use for the file chooser
      */
     public FileChooser(String path ,String dialogTitle, boolean selectFiles, boolean selectDirectories,
-                       boolean filterByExtension, String extension, boolean useCustomMessage, String customMessage, LocaleManager localeManager
+                       boolean filterByExtension, String extension, boolean useCustomMessage,
+                       String customMessage, LocaleManager localeManager
     ) {
         super();
         // Set file chooser gui properties
         setDialogTitle(dialogTitle);
-        setOpenDialogTitleText(dialogTitle);
-        setApproveButtonText(selectButtonText);
-        setCancelButtonText(cancelButtonText);
+
+        if (localeManager != null) {
+            this.localeManager = localeManager;
+
+            // if the locale does not contain the class, add it and it's components
+            if (!localeManager.getClassesInLocaleMap().contains("FileChooser")) {
+                addClassToLocale();
+            }
+            useLocale();
+        } else System.out.println("LocaleManager is null. FileChooser will launch without localization.");
+
         setPreferredSize(new Dimension(600, 450));
         setFileChooserFont(this.getComponents());
         SwingGUI.setHandCursorToClickableComponents(this);
 
         // Set the file chooser properties
-        int selectionMode
-                = selectFiles ? (selectDirectories ? JFileChooser.FILES_AND_DIRECTORIES : JFileChooser.FILES_ONLY)
-                : JFileChooser.DIRECTORIES_ONLY;
+        int selectionMode = selectFiles ?
+                ( selectDirectories
+                        ? JFileChooser.FILES_AND_DIRECTORIES
+                        : JFileChooser.FILES_ONLY ) : JFileChooser.DIRECTORIES_ONLY;
 
         setFileSelectionMode(selectionMode);
         setAcceptAllFileFilterUsed(false);
@@ -106,23 +154,112 @@ public class FileChooser extends JFileChooser {
             @Override
             public String getDescription() {
                 if (useCustomMessage) return customMessage;
-                if (filterByExtension) return extension.toUpperCase() + " Files (*." + extension + ")";
+                if (filterByExtension) return extension.toUpperCase() + extensionFilterDescription + " (*." + extension + ")";
 
-                if (selectionMode == JFileChooser.DIRECTORIES_ONLY) return "Directory (Folder)";
-                if (selectionMode == JFileChooser.FILES_ONLY) return "Any File";
-                return "Any File or Directory (Folder)";
+                if (selectionMode == JFileChooser.DIRECTORIES_ONLY) return directoryOnlyDescription;
+                if (selectionMode == JFileChooser.FILES_ONLY) return filesOnlyDescription;
+                return anyFileOrDirectory;
             }
         });
     }
 
-    private void setOpenDialogTitleText(String openDialogTitleText) {
-        UIManager.put("FileChooser.openDialogTitleText", openDialogTitleText);
+    private void addClassToLocale() {
+        Map<String, Map<String, String>> map = new TreeMap<>();
+            map.put("Main", new TreeMap<>());
+
+        Map<String, String> fileChooserMap = map.get("Main");
+            fileChooserMap.put("lookInLabelText", lookInLabelText);
+            fileChooserMap.put("saveInLabelText", saveInLabelText);
+            fileChooserMap.put("upFolderToolTipText", upFolderToolTipText);
+            fileChooserMap.put("homeFolderToolTipText", homeFolderToolTipText);
+            fileChooserMap.put("newFolderToolTipText", newFolderToolTipText);
+            fileChooserMap.put("listViewButtonToolTipText", listViewButtonToolTipText);
+            fileChooserMap.put("detailsViewButtonToolTipText", detailsViewButtonToolTipText);
+            fileChooserMap.put("fileNameHeaderText", fileNameHeaderText);
+            fileChooserMap.put("fileSizeHeaderText", fileSizeHeaderText);
+            fileChooserMap.put("fileTypeHeaderText", fileTypeHeaderText);
+            fileChooserMap.put("fileDateHeaderText", fileDateHeaderText);
+            fileChooserMap.put("fileAttrHeaderText", fileAttrHeaderText);
+            fileChooserMap.put("fileNameLabelText", fileNameLabelText);
+            fileChooserMap.put("filesOfTypeLabelText", filesOfTypeLabelText);
+            fileChooserMap.put("openButtonText", openButtonText);
+            fileChooserMap.put("openButtonToolTipText", openButtonToolTipText);
+            fileChooserMap.put("saveButtonText", saveButtonText);
+            fileChooserMap.put("saveButtonToolTipText", saveButtonToolTipText);
+            fileChooserMap.put("directoryOpenButtonText", directoryOpenButtonText);
+            fileChooserMap.put("directoryOpenButtonToolTipText", directoryOpenButtonToolTipText);
+            fileChooserMap.put("approveButtonText", approveButtonText);
+            fileChooserMap.put("cancelButtonText", cancelButtonText);
+            fileChooserMap.put("cancelButtonToolTipText", cancelButtonToolTipText);
+            fileChooserMap.put("newFolderErrorText", newFolderErrorText);
+            fileChooserMap.put("extFilterDescription", extensionFilterDescription);
+            fileChooserMap.put("dirOnlyDescription", directoryOnlyDescription);
+            fileChooserMap.put("filesOnlyDescription", filesOnlyDescription);
+            fileChooserMap.put("anyFileOrDirectory", anyFileOrDirectory);
+
+        localeManager.addClassSpecificMap("FileChooser", map);
     }
 
-    //TODO https://beradrian.wordpress.com/2007/07/30/internationalization-for-swing-standard-components/
+    private void useLocale() {
+        Map<String, String> varMap = localeManager.getAllVariablesWithinClassSpecificMap("FileChooser");
 
-    private void setCancelButtonText(String cancelButtonText) {
+        lookInLabelText = varMap.getOrDefault("lookInLabelText", lookInLabelText);
+        saveInLabelText = varMap.getOrDefault("saveInLabelText", saveInLabelText);
+        upFolderToolTipText = varMap.getOrDefault("upFolderToolTipText", upFolderToolTipText);
+        homeFolderToolTipText = varMap.getOrDefault("homeFolderToolTipText", homeFolderToolTipText);
+        newFolderToolTipText = varMap.getOrDefault("newFolderToolTipText", newFolderToolTipText);
+        listViewButtonToolTipText = varMap.getOrDefault("listViewButtonToolTipText", listViewButtonToolTipText);
+        detailsViewButtonToolTipText = varMap.getOrDefault("detailsViewButtonToolTipText", detailsViewButtonToolTipText);
+        fileNameHeaderText = varMap.getOrDefault("fileNameHeaderText", fileNameHeaderText);
+        fileSizeHeaderText = varMap.getOrDefault("fileSizeHeaderText", fileSizeHeaderText);
+        fileTypeHeaderText = varMap.getOrDefault("fileTypeHeaderText", fileTypeHeaderText);
+        fileDateHeaderText = varMap.getOrDefault("fileDateHeaderText", fileDateHeaderText);
+        fileAttrHeaderText = varMap.getOrDefault("fileAttrHeaderText", fileAttrHeaderText);
+        fileNameLabelText = varMap.getOrDefault("fileNameLabelText", fileNameLabelText);
+        filesOfTypeLabelText = varMap.getOrDefault("filesOfTypeLabelText", filesOfTypeLabelText);
+        openButtonText = varMap.getOrDefault("openButtonText", openButtonText);
+        openButtonToolTipText = varMap.getOrDefault("openButtonToolTipText", openButtonToolTipText);
+        saveButtonText = varMap.getOrDefault("saveButtonText", saveButtonText);
+        saveButtonToolTipText = varMap.getOrDefault("saveButtonToolTipText", saveButtonToolTipText);
+        directoryOpenButtonText = varMap.getOrDefault("directoryOpenButtonText", directoryOpenButtonText);
+        directoryOpenButtonToolTipText = varMap.getOrDefault("directoryOpenButtonToolTipText", directoryOpenButtonToolTipText);
+        approveButtonText = varMap.getOrDefault("approveButtonText", approveButtonText);
+        cancelButtonText = varMap.getOrDefault("cancelButtonText", cancelButtonText);
+        cancelButtonToolTipText = varMap.getOrDefault("cancelButtonToolTipText", cancelButtonToolTipText);
+        newFolderErrorText = varMap.getOrDefault("newFolderErrorText", newFolderErrorText);
+        extensionFilterDescription = varMap.getOrDefault("extFilterDescription", extensionFilterDescription);
+        directoryOnlyDescription = varMap.getOrDefault("dirOnlyDescription", directoryOnlyDescription);
+        filesOnlyDescription = varMap.getOrDefault("filesOnlyDescription", filesOnlyDescription);
+        anyFileOrDirectory = varMap.getOrDefault("anyFileOrDirectory", anyFileOrDirectory);
+
+        setLookAndFeel(true, false);
+        setApproveButtonText(approveButtonText);
+        UIManager.put("FileChooser.lookInLabelText", lookInLabelText);
+        UIManager.put("FileChooser.saveInLabelText", saveInLabelText);
+        UIManager.put("FileChooser.upFolderToolTipText", upFolderToolTipText);
+        UIManager.put("FileChooser.homeFolderToolTipText", homeFolderToolTipText);
+        UIManager.put("FileChooser.newFolderToolTipText", newFolderToolTipText);
+        UIManager.put("FileChooser.listViewButtonToolTipText", listViewButtonToolTipText);
+        UIManager.put("FileChooser.detailsViewButtonToolTipText", detailsViewButtonToolTipText);
+        UIManager.put("FileChooser.fileNameHeaderText", fileNameHeaderText);
+        UIManager.put("FileChooser.fileSizeHeaderText", fileSizeHeaderText);
+        UIManager.put("FileChooser.fileTypeHeaderText", fileTypeHeaderText);
+        UIManager.put("FileChooser.fileDateHeaderText", fileDateHeaderText);
+        UIManager.put("FileChooser.fileAttrHeaderText", fileAttrHeaderText);
+        UIManager.put("FileChooser.fileNameLabelText", fileNameLabelText);
+        UIManager.put("FileChooser.filesOfTypeLabelText", filesOfTypeLabelText);
+        UIManager.put("FileChooser.openButtonText", openButtonText);
+        UIManager.put("FileChooser.openButtonToolTipText", openButtonToolTipText);
+        UIManager.put("FileChooser.saveButtonText", saveButtonText);
+        UIManager.put("FileChooser.saveButtonToolTipText", saveButtonToolTipText);
+        UIManager.put("FileChooser.directoryOpenButtonText", directoryOpenButtonText);
+        UIManager.put("FileChooser.directoryOpenButtonToolTipText", directoryOpenButtonToolTipText);
         UIManager.put("FileChooser.cancelButtonText", cancelButtonText);
+        UIManager.put("FileChooser.cancelButtonToolTipText", cancelButtonToolTipText);
+        UIManager.put("FileChooser.newFolderErrorText", newFolderErrorText);
+
+        SwingUtilities.updateComponentTreeUI(this);
+
     }
 
     private void setFileChooserFont(Component[] comp) {
