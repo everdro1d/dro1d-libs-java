e81b4ba255ae3ffc44c83914fb6205e081fa2868
everdro1d <everdro1d-github@pm.me>
2025-04-30 10:10:51 -1000

    chore [Swingtestbench]: update locale manager implementation

--- src/com/everdro1d/libs/swing/test/SwingTestBench.java
+++ src/com/everdro1d/libs/swing/test/SwingTestBench.java
@@ -26,7 +26,7 @@ public class SwingTestBench {
             SwingGUI.uiSetup("Tahoma", 18);
             System.out.println("DarkMode?" + SwingGUI.isDarkModeActive());
 
-            LocaleManager localeManager = new LocaleManager(SwingTestBench.class);
+            LocaleManager localeManager = new LocaleManager();
             localeManager.loadLocaleFromFile(p.get("currentLocale","eng"));
 
             JPanel panel = new JPanel();
