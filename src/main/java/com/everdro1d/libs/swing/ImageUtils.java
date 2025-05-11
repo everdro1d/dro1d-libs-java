50ace1308d4e6cadb2dfaac6e9e7de02eec26eac
everdro1d <everdro1d-github@pm.me>
2025-05-10 20:46:48 -0600

    refactor [SwingGUI]: split image related methods into ImageUtils class

---
+++ src/main/java/com/everdro1d/libs/swing/ImageUtils.java
@@ -0,0 +1,82 @@
+// dro1dDev - created: 2025-05-10
+
+package com.everdro1d.libs.swing;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.InputStream;
+
+public class ImageUtils {
+
+    /**
+     * Set the icon of the frame.
+     * @param frame the frame to set the icon of
+     * @param internalPath path to the image file within resources
+     * @param clazz the class to trace from
+     */
+    public static void setFrameIcon(JFrame frame, String internalPath, Class<?> clazz) {
+        Icon icon = getApplicationIcon(internalPath, clazz);
+        if (icon != null) {
+            frame.setIconImage(((ImageIcon) icon).getImage());
+        }
+    }
+
+    /**
+     * Retrieve an image as an Icon from the application's resources.
+     * @param internalPath path to the image file within resources
+     * @param clazz the class to trace from
+     * @return the Icon
+     */
+    public static Icon getApplicationIcon(String internalPath, Class<?> clazz) {
+        Icon icon = null;
+        try (InputStream iconStream = clazz.getClassLoader().getResourceAsStream(internalPath)) {
+            if (iconStream != null) {
+                icon = new ImageIcon(ImageIO.read(iconStream));
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+        if (icon == null) {
+            System.err.println("[ERROR] Could not find icon file at: " + internalPath);
+        }
+        return icon;
+    }
+
+    public static ImageIcon changeIconColor(Icon icon, Color color) {
+        // Create a new image that can be edited
+        BufferedImage bufferedImage = new BufferedImage(
+                icon.getIconWidth(),
+                icon.getIconHeight(),
+                BufferedImage.TYPE_INT_ARGB
+        );
+        Graphics2D g2d = bufferedImage.createGraphics();
+        // Draw the icon on the new image
+        icon.paintIcon(null, g2d, 0, 0);
+        g2d.dispose();
+
+        // Create a filter that changes the color of the image
+        ImageFilter filter = new RGBImageFilter() {
+            public int filterRGB(int x, int y, int rgb) {
+                // If the pixel is transparent, preserve its original RGB values
+                if ((rgb & 0xFF000000) == 0) {
+                    return rgb;
+                }
+                // If the pixel is not transparent, replace its RGB values with the desired color
+                else {
+                    // Preserve the alpha value
+                    int alpha = rgb & 0xFF000000;
+                    // Replace the RGB values with the desired color
+                    return alpha | (color.getRGB() & 0x00FFFFFF);
+                }
+            }
+        };
+
+        // Apply the filter
+        ImageProducer producer = new FilteredImageSource(bufferedImage.getSource(), filter);
+        Image newImage = Toolkit.getDefaultToolkit().createImage(producer);
+
+        return new ImageIcon(newImage);
+    }
+}
